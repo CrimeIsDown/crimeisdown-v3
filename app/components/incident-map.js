af92614d602b30a9de41770c9374d130acc5800d
Eric Tendian <erictendian@gmail.com>
2016-06-10 20:34:12 -0500

    Adding GeoJSON for police districts, adding other data from CrimeIsDown 2.0

--- app/components/incident-map.js
+++ app/components/incident-map.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
 
+let defaultGeoJSON = {
+  type: 'FeatureCollection',
+  features: []
+};
+
 export default Ember.Component.extend({
   lat: 41.8781,
   lng: -87.6298,
-  zoom: 10
+  zoom: 10,
+  policeDistricts: defaultGeoJSON,
+  didInsertElement() {
+    Ember.$.getJSON("/map_data/police_districts.json").done((data) => {
+      this.set('policeDistricts', data)
+    });
+  }
 });
