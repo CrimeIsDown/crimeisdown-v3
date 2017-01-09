db10dac520604c8ff6813bfc44b2270e31f2f062
Eric Tendian <erictendian@gmail.com>
2017-01-08 23:37:25 -0600

    Fixing CORS issues, HTML in map page

--- app/components/search-ucrcodes.js
+++ app/components/search-ucrcodes.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   ucrCodes: [],
   ucr: {},
   didInsertElement() {
-    Ember.$.getJSON('https://script.google.com/macros/s/AKfycbwdMu3lgUaMPqA-ESlmhD12Yo6Jz78LlCM8cMQXW7Cm4O94sAA/exec?id=1Zzx6UXOYL5BXXYTO_PanTESGS5nHLHDMxBi7u0k1ppg&sheet=UCR%20Codes')
+    Ember.$.getJSON('https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbwdMu3lgUaMPqA-ESlmhD12Yo6Jz78LlCM8cMQXW7Cm4O94sAA/exec?id=1Zzx6UXOYL5BXXYTO_PanTESGS5nHLHDMxBi7u0k1ppg&sheet=UCR%20Codes')
       .done((data) => {
         this.ucrCodes = data['UCR Codes'];
       })
