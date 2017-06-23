b5d1e5f2868243bf071e5d253673944498d7734e
Eric Tendian <erictendian@gmail.com>
2017-06-22 22:24:45 -0500

    Adding glyphicons to build so datepicker shows up properly

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -72,5 +72,11 @@ module.exports = function(defaults) {
   app.import('vendor/font-awesome/fonts/fontawesome-webfont.woff', {destDir: 'assets/fonts'});
   app.import('vendor/font-awesome/fonts/fontawesome-webfont.woff2', {destDir: 'assets/fonts'});
 
+  app.import('vendor/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.eot', {destDir: 'assets/fonts'});
+  app.import('vendor/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.svg', {destDir: 'assets/fonts'});
+  app.import('vendor/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.ttf', {destDir: 'assets/fonts'});
+  app.import('vendor/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff', {destDir: 'assets/fonts'});
+  app.import('vendor/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff2', {destDir: 'assets/fonts'});
+
   return app.toTree();
 };
