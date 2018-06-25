fb5bf27f06c45866665b7e4fb83cc4f7e5af71c1
Eric Tendian <erictendian@gmail.com>
2018-06-24 20:50:23 -0500

    Login anonymously so data can be accessed

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import { inject as service } from '@ember/service';
+import { getOwner } from '@ember/application';
 
-export default Route.extend(ApplicationRouteMixin);
+export default Route.extend(ApplicationRouteMixin, {
+  session: service('session'),
+
+  init() {
+    if (!this.get('session').isAuthenticated) {
+      getOwner(this).lookup('authenticator:firebase').get('firebase')
+        .auth().signInAnonymously().catch(function(error) {
+          console.error(error)
+        });
+    }
+  }
+});
