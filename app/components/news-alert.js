840f93cd6574b01284b5cbe368d11ba32637d54c
Eric Tendian <erictendian@gmail.com>
2018-12-23 20:51:14 -0600

    Adding loading message and info bar about the site updates

--- app/components/news-alert.js
+++ app/components/news-alert.js
@@ -1,4 +1,16 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  init() {
+    this._super(...arguments);
+    this.localStorageKey = 'saw-alert-20181223';
+    this.set('hasSeenAlert', localStorage.getItem(this.localStorageKey) !== null);
+  },
+  didInsertElement() {
+    if (!this.hasSeenAlert) {
+      $('#news-alert').on('closed.bs.alert', function () {
+        localStorage.setItem(this.localStorageKey, true);
+      });
+    }
+  }
 });
