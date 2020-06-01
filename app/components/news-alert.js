669752b2496bacab70dee7bcfe76d91b3e82a721
Eric Tendian <erictendian@gmail.com>
2020-05-31 21:18:32 -0500

    Revert "Adding alert related to protests"
    This reverts commit 7d9f3ca642f8a2d70687bdb0d2168c29300e9435.

--- app/components/news-alert.js
+++ app/components/news-alert.js
@@ -3,21 +3,21 @@ import { run } from '@ember/runloop';
 import $ from 'jquery';
 
 export default class NewsAlert extends Component {
-  hasSeenAlert = false;
+  hasSeenAlert = true;
 
   constructor() {
     super(...arguments);
-    try {
-      this.hasSeenAlert = localStorage.getItem('saw-alert-20200530') !== null;
+    /*try {
+      this.hasSeenAlert = localStorage.getItem('saw-alert-20181223') !== null;
     } catch (e) {
       return false;
-    }
+    }*/
 
     if (!this.hasSeenAlert) {
       $('#news-alert').on('closed.bs.alert', () => {
         run(() => {
           try {
-          localStorage.setItem('saw-alert-20200530', true);
+          localStorage.setItem('saw-alert-20181223', true);
           } catch (e) {
             return false;
           }
