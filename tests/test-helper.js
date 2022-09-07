8b37cd6a562b0208f67437bc3b3b11f729ade681
Eric Tendian <erictendian@gmail.com>
2022-09-06 19:26:42 -0500

    Fixing test helper

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from 'my-app/app';
-import config from 'my-app/config/environment';
+import Application from 'crimeisdown/app';
+import config from 'crimeisdown/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
