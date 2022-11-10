0bd875cd140d07003f015cf7c387a20c507cd7cb
Eric Tendian <erictendian@gmail.com>
2022-11-10 15:34:58 -0600

    Fixing linting, bug

--- app/components/virtual-scanner.js
+++ app/components/virtual-scanner.js
@@ -85,7 +85,10 @@ export default class VirtualScanner extends Component {
               'https://www.broadcastify.com/listen/feed/' +
               stream.broadcastifyId;
           }
-          if (!this.streams.find((streamObj) => streamObj.slug === stream.slug) && (stream.broadcastify || stream.name.startsWith('CFD'))) {
+          if (
+            !this.streams.find((streamObj) => streamObj.name == stream.slug) &&
+            (stream.broadcastify || stream.name.startsWith('CFD'))
+          ) {
             this.streams.pushObject({
               name: stream.slug,
               desc: stream.shortname ?? stream.name,
