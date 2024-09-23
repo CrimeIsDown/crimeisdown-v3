8bf05b8ef2c60262de229fd354744cfa09df8fc9
Eric Tendian <erictendian@gmail.com>
2024-09-22 23:50:08 -0500

    Add ignore_keywords support

--- app/models/transcript-subscription.js
+++ app/models/transcript-subscription.js
@@ -3,6 +3,7 @@ import Model, { attr } from '@ember-data/model';
 export default class TranscriptSubscriptionModel extends Model {
   @attr name;
   @attr keywords;
+  @attr ignore_keywords;
   @attr location;
   @attr topic;
   @attr notification_channels;
@@ -17,6 +18,20 @@ export default class TranscriptSubscriptionModel extends Model {
       }
     }
     return Array.isArray(keywords) ? keywords.join('\n') : keywords;
+  }
+
+  get ignoreKeywordsString() {
+    let ignore_keywords = this.ignore_keywords;
+    if (typeof this.keywords === 'string') {
+      try {
+        ignore_keywords = JSON.parse(this.ignore_keywords);
+      } catch {
+        // Do nothing
+      }
+    }
+    return Array.isArray(ignore_keywords)
+      ? ignore_keywords.join('\n')
+      : ignore_keywords;
   }
 
   get topics() {
